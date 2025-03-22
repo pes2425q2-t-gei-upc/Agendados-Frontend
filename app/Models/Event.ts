3bf1c1a8415e227702c9a3e320d2f9b30d2828b1
Adria Guilera <adriaguilera7@gmail.com>
2025-03-22 21:23:38 +0100

    Interface from response

--- app/Models/Event.ts
+++ app/Models/Event.ts
@@ -1,34 +1,63 @@
-export interface Coordinate {
-  latitude: number;
-  longitude: number;
+interface EventDTO {
+  id: number;
+  title: string;
+  description?: string;
+  date_ini: string;
+  date_end: string;
+  info_tickets?: string;
+  schedule?: string;
+  categories: Category[];
+  scopes?: any[];
+  location?: Location;
+  images: Image[];
+  links: Link[];
+}
+interface Link {
+  link: string;
+}
+interface Image {
+  image_url: string;
+}
+interface Location {
+  id: number;
+  region: Category;
+  town: Category;
+  latitude?: number;
+  longitude?: number;
+  address?: string;
+  space?: string;
+}
+interface Category {
+  id: number;
+  name: string;
 }
 
-export interface EventDTO {
-  id: string;
+class Event implements EventDTO {
+  id: number;
   title: string;
-  location: string;
-  data: Date;
-  coverImage: string;
-  categoria: string;
-  coordinate?: Coordinate;
-}
+  description?: string;
+  date_ini: string;
+  date_end: string;
+  info_tickets?: string;
+  schedule?: string;
+  categories: Category[];
+  scopes?: any[];
+  location?: Location;
+  images: Image[];
+  links: Link[];
 
-export class Event implements EventDTO {
-  id: string;
-  title: string;
-  location: string;
-  data: Date;
-  coverImage: string;
-  categoria: string;
-  coordinate?: Coordinate;
-
-  constructor(dto: EventDTO) {
-    this.id = dto.id;
-    this.title = dto.title;
-    this.coverImage = dto.coverImage;
-    this.location = dto.location;
-    this.data = dto.data;
-    this.categoria = dto.categoria;
-    this.coordinate = dto.coordinate;
+  constructor(event: EventDTO) {
+    this.id = event.id;
+    this.title = event.title;
+    this.description = event.description;
+    this.date_ini = event.date_ini;
+    this.date_end = event.date_end;
+    this.info_tickets = event.info_tickets;
+    this.schedule = event.schedule;
+    this.categories = event.categories;
+    this.scopes = event.scopes;
+    this.location = event.location;
+    this.images = event.images;
+    this.links = event.links;
   }
 }
