48060216f0a4c6c09543f00eb8cbc42c1129b93d
arnau.farran <arnau.farran@estudiantat.upc.edu>
2025-05-09 00:02:31 +0200

    Fix index in deploy

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 // app/_layout.tsx
 import { Stack } from 'expo-router';
 import React, { useEffect } from 'react';
+import { SafeAreaView } from 'react-native';
 
 import { FavoritesProvider } from '@context/FavoritesContext';
 import { FriendshipProvider } from '@context/FriendshipContext';
@@ -20,41 +21,43 @@ export default function RootLayout() {
       <EventsProvider>
         <FavoritesProvider>
           <FriendshipProvider>
-            <Stack>
-              <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-              <Stack.Screen
-                name='registerLogin'
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name='chat'
-                options={{
-                  headerShown: false,
-                  animation: 'slide_from_right',
-                  presentation: 'card',
-                }}
-              />
-              <Stack.Screen
-                name='friends'
-                options={{ headerShown: true, title: 'Amigos' }}
-              />
-              <Stack.Screen
-                name='friendEvents'
-                options={{
-                  headerShown: true,
-                  title: 'Eventos guardados por amigo',
-                }}
-              />
-              <Stack.Screen
-                name='config'
-                options={{
-                  headerShown: false, // Ocultamos el header por defecto
-                  title: 'Configuración', // Este título no se verá porque headerShown es false
-                  animation: 'slide_from_right', // Animación lateral
-                  presentation: 'card', // Presentación normal de tipo tarjeta
-                }}
-              />
-            </Stack>
+            <SafeAreaView style={styles.safeArea}>
+              <Stack>
+                <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+                <Stack.Screen
+                  name='registerLogin'
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name='chat'
+                  options={{
+                    headerShown: false,
+                    animation: 'slide_from_right',
+                    presentation: 'card',
+                  }}
+                />
+                <Stack.Screen
+                  name='friends'
+                  options={{ headerShown: true, title: 'Amigos' }}
+                />
+                <Stack.Screen
+                  name='friendEvents'
+                  options={{
+                    headerShown: true,
+                    title: 'Eventos guardados por amigo',
+                  }}
+                />
+                <Stack.Screen
+                  name='config'
+                  options={{
+                    headerShown: false,
+                    title: 'Configuración',
+                    animation: 'slide_from_right',
+                    presentation: 'card',
+                  }}
+                />
+              </Stack>
+            </SafeAreaView>
           </FriendshipProvider>
         </FavoritesProvider>
       </EventsProvider>
