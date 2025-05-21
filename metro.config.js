ab28ab143b436c7c420df97a0aa5384e30be2fad
arnau.farran <arnau.farran@estudiantat.upc.edu>
2025-05-21 16:53:47 +0200

    Try apk

--- metro.config.js
+++ metro.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push('svg'); // Agregar "svg" como una extensión para los assets
-config.resolver.sourceExts.push('svg'); // Agregar "svg" como una extensión para los source files
+config.resolver.sourceExts = ['js', 'jsx', 'ts', 'tsx', 'json', 'svg', ...config.resolver.sourceExts.filter(ext => !['js', 'jsx', 'ts', 'tsx', 'json', 'svg'].includes(ext))];
 
 config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer'); // Usar el transformador SVG
 
